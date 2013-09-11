2fd90e1e7d097ee4168aafe1f750e2a9fe82e958
Derik Strattan <derik.strattan@gmail.com>
2013-09-10 21:47:47 -0700

    Project finished for addresses and users

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CfPersons::Application.config.secret_key_base = '629bd0e91c44e490105034b5fdd6fd27574be04a352da67866934bd394d27a2d89977c5be54b4159f00651eba17bb874a63dcf528e32113ad6c3b4c2d97c8321'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CfPersons::Application.config.session_store :cookie_store, key: '_cf-persons_session'
