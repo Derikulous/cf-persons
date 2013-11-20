5eced5db1d4f7cc8b1e7a7760aafc2cd7997f8c6
Derik Strattan <derik.strattan@gmail.com>
2013-11-19 23:30:07 -0800

    Added simple form and template inheritance

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,7 @@
 class Person < ActiveRecord::Base
   has_one :address
-
   accepts_nested_attributes_for :address
-
+  validates_presence_of :first_name
   def name
     [first_name, last_name].join(' ')
   end
